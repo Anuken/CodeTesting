9e869acb33d40f98de2def2292123b8667e2f6cc
BuildTools <arnukren@gmail.com>
2016-02-15 14:59:13 -0500

    Initial gradle setup.

---
+++ desktop/src/net/pixelstatic/codetesting/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.pixelstatic.codetesting.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.pixelstatic.codetesting.CodeTester;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CodeTester(), config);
+	}
+}
